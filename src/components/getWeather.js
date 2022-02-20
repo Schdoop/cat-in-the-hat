94540e96337260c104b0e8d7c3f46d004257e1c6
Oleh Korniienko <oleh.korniienko@sisense.com>
2022-02-20 16:02:39 +0200

    Code improvements

--- src/components/getWeather.js
+++ src/components/getWeather.js
@@ -1,5 +1,5 @@
 async function getWeather() {
-    const result = await fetch('http://localhost:3000/weather');
+    const result = await fetch('https://schdoop.dev:8443/weather');
     const json = await result.json();
     return json.data.timelines[0].intervals;
 }
